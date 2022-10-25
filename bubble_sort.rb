4b7e7a9601f50addc6987a0fb91d8ff666100765
Robert <doubtfullink53@gmail.com>
2022-10-25 11:08:39 -0400

    reduce if statements and make flexible for more than 5 numbers

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,36 +3,29 @@
 def bubble_sort (numbers)
 
     $i = 0
-    $num = 200
+    $num = numbers.length 
 
 while $i < $num do
  numbers.each_with_index do |number, index|
-  
-
-    
-        if(numbers[0] > numbers[1])
-            numbers[0], numbers[1] = numbers[1], numbers[0]
+    index2 = 1
+    index2 =  index + index2
+      
+        if(!numbers[index2].nil? )
+            if(numbers[index] > numbers[index2] )
+                numbers[index], numbers[index2] = numbers[index2], numbers[index]
+            end
         end
 
-        if(numbers[1] > numbers[2])
-            numbers[1], numbers[2] = numbers[2], numbers[1]
-        end
-        if(numbers[2] > numbers[3])
-            numbers[2], numbers[3] = numbers[3], numbers[2]
-        end
-        if(numbers[3] > numbers[4])
-            numbers[3], numbers[4] = numbers[4], numbers[3]
-        end
-        if(numbers[4] > numbers[5])
-            numbers[4], numbers[5] = numbers[5], numbers[4]
-        end
-        
     end
     $i +=1
  end
- numbers
 
+numbers
 end
 
 
  p bubble_sort([4,3,78,2,0,2])
+
+
+
+ # >> [0, 2, 2, 3, 4, 78]
